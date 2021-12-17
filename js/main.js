24ce8f2b9dc2c484c01f64afa859ecd2024ea0d9
seanny3 <dev.seann@gmail.com>
2021-12-17 12:00:00 +0900

    Day 01

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const more_comment = document.querySelector('.comments__container');
+const popup = document.querySelector('.comments__popup')
+const header = document.querySelector('.header');
+more_comment.addEventListener('click',() => {
+    header.style.display ="none";
+    
+    popup.classList.add('slideUp');
+})
+
+const quit_popup = document.querySelectorAll('.popup__metadata__right i')[1];
+quit_popup.addEventListener('click', () => {
+    header.style.display ="flex";
+    popup.classList.remove('slideUp');
+})
+
