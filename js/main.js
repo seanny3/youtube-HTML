64ede1ac63487fe9d29f702d6d94655340bbf0d0
seanny3 <dev.seann@gmail.com>
2021-12-18 12:00:00 +0900

    Day 01-2

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@
 const more_comment = document.querySelector('.comments__container');
 const popup = document.querySelector('.comments__popup')
 const header = document.querySelector('.header');
+const quit_popup = document.querySelectorAll('.popup__metadata__right i')[1];
+const video_bottom = document.querySelector('.video__bottom');
+
 more_comment.addEventListener('click',() => {
-    header.style.display ="none";
-    
+    header.classList.add('invisible');
+    video_bottom.classList.add('unScroll');
     popup.classList.add('slideUp');
 })
 
-const quit_popup = document.querySelectorAll('.popup__metadata__right i')[1];
 quit_popup.addEventListener('click', () => {
-    header.style.display ="flex";
+    header.classList.remove('invisible');
+    video_bottom.classList.remove('unScroll');
     popup.classList.remove('slideUp');
 })
 
